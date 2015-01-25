5b32bbc894e4d2aa2786da4569bec34b3fb396fe
Michele Memoli <michele@100shapes.com>
2015-01-25 17:50:18 +0000

    Added elasticsearch

--- opencourts/settings.py
+++ opencourts/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # libs
+    'haystack',
+
+    # Apps
     'courts',
 )
 
@@ -88,3 +92,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Haystack Search
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 40
