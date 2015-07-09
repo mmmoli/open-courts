86bf686e00af10182d38a8c769d7b3feb566dc41
Michele Memoli <michele@100shapes.com>
2015-07-09 23:12:24 +0100

    Added ability to get location from device

--- client/controllers/CourtsFindCtrl.js
+++ client/controllers/CourtsFindCtrl.js
@@ -1,9 +1,14 @@
 var ngModule = angular.module('open_courts');
 
-function CourtsFindCtrl($meteor) {
+function CourtsFindCtrl($meteor, $log) {
     var vm = this;
     vm.courts = $meteor.collection(Courts);
+
+    vm.getLocation = function () {
+        var location = Geolocation.latLng() || { lat: 0, lng: 0 };
+        $log.log(location);
+    }
 }
 
-ngModule.controller('CourtsFindCtrl', ['$meteor', CourtsFindCtrl]);
+ngModule.controller('CourtsFindCtrl', ['$meteor', '$log', CourtsFindCtrl]);
 
